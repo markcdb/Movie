00d672a923196fa786ef01628cb93730f11589b0
Mark Christian Buot <mark_cdb@yahoo.com>
2019-07-14 12:47:56 +0800

    * Removed pop, Removed provisioning profiles

--- Movie/Helpers/ViewHelper.swift
+++ Movie/Helpers/ViewHelper.swift
@@ -8,22 +8,8 @@
 
 import Foundation
 import UIKit
-import pop
 
 extension UIView {
-    
-    func bounce(withCompletion completion: ((POPAnimation?, Bool) -> Void)?) {
-        
-        let springAnimation = POPSpringAnimation(propertyNamed: kPOPViewScaleXY)
-        springAnimation?.fromValue = NSValue(cgPoint: CGPoint(x: 0.5, y: 0.5))
-        springAnimation?.toValue = NSValue(cgPoint: CGPoint(x: 1, y: 1))
-        springAnimation?.velocity = NSValue(cgPoint: CGPoint(x: 8, y: 8))
-        springAnimation?.springBounciness = 20
-        springAnimation?.completionBlock = completion
-        springAnimation?.springSpeed = 20
-        
-        self.pop_add(springAnimation, forKey: "springAnimation")
-    }
     
     func setShadow() {
         
--- Pods/Target Support Files/Pods-Movie/Pods-Movie-frameworks.sh
+++ Pods/Target Support Files/Pods-Movie/Pods-Movie-frameworks.sh
@@ -148,7 +148,6 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/RxCocoa/RxCocoa.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxRelay/RxRelay.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxSwift/RxSwift.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/pop/pop.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Kingfisher/Kingfisher.framework"
@@ -156,7 +155,6 @@ if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/RxCocoa/RxCocoa.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxRelay/RxRelay.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxSwift/RxSwift.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/pop/pop.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
